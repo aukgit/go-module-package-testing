697db791aa8899e27b530ec795cae1733ac6421a
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-05 23:58:00 +0600

    # Updated and Fixed Code
    - run `localhost:8080/hello/alim`
    - run `localhost:8080` will log in console:
       `{"level":"info","ts":1596650175.2169445,"caller":"go-module-package-testing/main.go:14","msg":"Success"}
    `

--- main.go
+++ main.go
@@ -14,9 +14,14 @@ func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	logger.Info("Success")
 }
 
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
 func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
 	fmt.Println("Hello World")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
