03ff758820aa8c09e3ea9a8f19477735351b9dc1
Md. Alim Ul Karim <devorg.bd@gmail.com>
2020-08-05 23:45:32 +0600

    # Updated packages, added go modules
    - sample code added
    
    ```batch
    go mod init github.com/aukgit/go-module-package-testing
    go get github.com/julienschmidt/httprouter
    go get go.uber.org/zap
    go mod tidy
    ```

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
-	"fmt"
 
 	"github.com/julienschmidt/httprouter"
 	"go.uber.org/zap"
 )
 
 func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	logger, _ = zap.NewProduction()
+	logger, _ := zap.NewProduction()
 	logger.Info("Success")
 }
 
 func main() {
-	router  : = httprouter.New()
+	router := httprouter.New()
 	router.GET("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
 	fmt.Println("Hello World")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
